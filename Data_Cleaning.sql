195aaf30e5538a110e401a838cc8e24d1b33a424
DiwamG <gosaidiwam@gmail.com>
2024-12-31 22:52:17 +0530

    Data_Cleaning.sql

--- Data_Cleaning.sql
+++ Data_Cleaning.sql
@@ -1,12 +1,5 @@
 -- SQL Project - Data Cleaning
 
--- https://www.kaggle.com/datasets/swaptr/layoffs-2022
-
-
-
-
-
-
 SELECT * 
 FROM world_layoffs.layoffs;
 
@@ -21,16 +14,10 @@ SELECT * FROM world_layoffs.layoffs;
 
 
 -- now when we are data cleaning we usually follow a few steps
--- 1. check for duplicates and remove any
--- 2. standardize data and fix errors
--- 3. Look at null values and see what 
--- 4. remove any columns and rows that are not necessary - few ways
-
-
 
 -- 1. Remove Duplicates
 
-# First let's check for duplicates
+z-- First let's check for duplicates
 
 
 
